0008c39e9e046421c9fe46a6a188a7d4b21fe98a
cmd-AJ <andrewlol774@gmail.com>
2026-03-23 14:49:21 -0600

    added: removed previously before files

--- Ar_zoo/src/custom-element.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'a-scene': any;
-    'a-entity': any;
-    'a-camera': any;
-    'a-gltf-model': any;
-    [key: string]: any; // This allows any a-frame element
-  }
-}
