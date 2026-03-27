53a2750f1d79ec58c329b1298073a8bd1421f63f
cmd-AJ <andrewlol774@gmail.com>
2026-03-27 10:14:20 -0600

    fixed planets issue with position

--- Cayalosfera/src/components/ARScene.jsx
+++ Cayalosfera/src/components/ARScene.jsx
@@ -153,7 +153,7 @@ export default function ARScene({ path, animalPaths = [] }) {
                 <a-gltf-model
                   src="/modelos/uranus.glb"
                   scale="0.2 0.2 0.2"
-                  position="0 0.5 0"
+                  position="0 0.2 0"
                   rotation="0 0 0"
                   animation="property: rotation; to: 0 360 0; loop: true; dur: 16000; easing: linear"
 
